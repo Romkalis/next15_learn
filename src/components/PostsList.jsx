0b04b64f9d2164ad4bda091f39b0e4f896d87f82
Roman Bondarchuk <lisss17@mail.ru>
2025-02-13 15:48:33 +0100

    added simply server, useEffect & changed saving to backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,58 +1,70 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
-import {useState} from "react";
 
 import s from "./PostsList.module.css";
 
 const PostsList = ({modalIsVisible, onCloseModal}) => {
 
-  const isLocalStateData = () => {
-    try {
-      return JSON.parse(localStorage.getItem('posts')) || []
-      // проверка - если дата невалидная, то вернет пустой массив, если нет свойства - ошибка, где вернем пустой массив
-    } catch (e) {
-      return []
-    }
-  }
+  const [posts, setPosts] = useState([])
 
-  const [posts, setPosts] = useState(isLocalStateData())
+
+  useEffect(() => {
+    // fetch('http://localhost:8080/posts')
+    //   .then( response =>  response.json() )
+    //   .then ( data => {
+    //     console.log(data)
+    //     setPosts(data.posts)
+    //   })
+    //   .catch( e => console.warn('Fetch error' ))
+    (async function () {
+      try {
+        const response = await fetch('http://localhost:8080/posts')
+        if (!response.ok) throw new Error('We had not have response');
+        const data = await response.json()
+        setPosts(data?.posts)
+      } catch (e) {
+        console.warn('Fetched from server failed', e.message)
+      }
+
+    })()
+
+  }, []);
 
   const onPostSubmit = (name, text) => {
-    const newPost = [...posts, {author: name, text: text}]
-    setPosts(newPost)
-    localStorage.setItem( "posts",  JSON.stringify(newPost))
+
+    const newPost = {author: name, body: text}
+
+    fetch('http://localhost:8080/posts', {
+      method: "POST", body: JSON.stringify(newPost), headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const updatedPosts = [...posts, newPost]
+    setPosts(updatedPosts)
   }
 
-  return (
-    <>
-      {posts.length > 0 &&
-      <ul className={s.posts}>
-        {posts.map(post => <Post
-          author={post.author}
-          body={post.text}
-          key={post.text + post.author}
-        />)}
-      </ul>
-      }
+  return (<>
+    {posts.length > 0 && <ul className={s.posts}>
+      {posts.map(post => <Post
+        author={post.author}
+        body={post.body}
+        key={post.id || post.body}
+      />)}
+    </ul>}
 
-      {posts.length === 0 &&
-      <>
-        <h2>There are not posts yet </h2>
-        <p>Start adding some!</p>
-      </>}
-
-      {modalIsVisible &&
-        <Modal onClose={onCloseModal}>
-          <NewPost
-            onCancel={onCloseModal}
-            onPosted={onPostSubmit}
-          />
-        </Modal>
-      }
-    </>
-  );
+    {posts.length === 0 && <>
+      <h2>There are not posts yet </h2>
+      <p>Start adding some!</p>
+    </>}
+    {modalIsVisible && <Modal onClose={onCloseModal}>
+      <NewPost
+        onCancel={onCloseModal}
+        onPosted={onPostSubmit}
+      />
+    </Modal>}
+  </>);
 };
 
 export default PostsList;
